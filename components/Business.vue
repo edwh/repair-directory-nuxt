1f6f6bfc64f0d2f87a89a3ceea7aba5e89646973
Edward Hibbert <edward@ehibbert.org.uk>
2021-01-14 14:36:57 +0000

    Share button on individual business

--- components/Business.vue
+++ components/Business.vue
@@ -1,55 +1,70 @@
 <template>
-  <div
-    :class="{
-      business: true,
-      faded: selected && selected !== business.uid,
-    }"
-    @click="select"
-  >
-    <div ref="heading" class="business__heading">
-      <h2 class="name">{{ business.name }}</h2>
-      <div class="business__positive-review-percentage">
-        <h2 class="percentage font-weight-bold">100%</h2>
-        <span>positive reviews</span>
+  <div>
+    <div
+      :class="{
+        business: true,
+        faded: selected && selected !== business.uid,
+      }"
+      @click="select"
+    >
+      <div ref="heading" class="business__heading">
+        <h2 class="name">{{ business.name }}</h2>
+        <div class="business__positive-review-percentage">
+          <h2 class="percentage font-weight-bold">100%</h2>
+          <span>positive reviews</span>
+        </div>
       </div>
-    </div>
-    <div class="business__details">
-      <div class="row">
-        <div class="col-md-12">
-          <p class="business-detail">
-            <v-icon name="globe" class="icon" />
-            <a
-              target="_blank"
-              rel="noopener"
-              :href="business.website"
-              @click="trackOutboundLink(business.website)"
-            >
-              {{ business.website }}
-            </a>
-          </p>
+      <div class="business__details">
+        <div class="row">
+          <div class="col-md-12">
+            <p class="business-detail">
+              <v-icon name="globe" class="icon" />
+              <a
+                target="_blank"
+                rel="noopener"
+                :href="business.website"
+                @click="trackOutboundLink(business.website)"
+              >
+                {{ business.website }}
+              </a>
+            </p>
 
-          <p class="business-detail">
-            <v-icon name="phone" class="icon" />
-            <a
-              :href="'tel:' + business.landline"
-              rel="noopener"
-              @click="trackOutboundLink('tel:' + business.landline)"
-            >
-              {{ business.landline }}
-            </a>
-          </p>
+            <p class="business-detail">
+              <v-icon name="phone" class="icon" />
+              <a
+                :href="'tel:' + business.landline"
+                rel="noopener"
+                @click="trackOutboundLink('tel:' + business.landline)"
+              >
+                {{ business.landline }}
+              </a>
+            </p>
 
-          <p class="business-detail">
-            <v-icon name="map-marker" class="icon" />
-            <span>{{ business.address }}</span>
-          </p>
+            <p class="business-detail">
+              <v-icon name="map-marker" class="icon" />
+              <span>{{ business.address }}</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
+    <div class="d-flex justify-content-end">
+      <b-btn variant="link" @click="share">
+        Share business
+        <v-icon name="share" />
+      </b-btn>
+    </div>
+    <ShareBusinessModal
+      v-if="showShareModal"
+      ref="shareModal"
+      :business="business"
+    />
   </div>
 </template>
 <script>
+import ShareBusinessModal from '@/components/ShareBusinessModal'
 export default {
+  components: { ShareBusinessModal },
   props: {
     business: {
       type: Object,
@@ -60,6 +75,11 @@ export default {
       required: false,
       default: null,
     },
+  },
+  data() {
+    return {
+      showShareModal: false,
+    }
   },
   computed: {},
   watch: {
@@ -80,6 +100,13 @@ export default {
   methods: {
     select() {
       this.$emit('select', this.business.uid)
+    },
+    share() {
+      this.showShareModal = true
+
+      this.waitForRef('shareModal', () => {
+        this.$refs.shareModal.show()
+      })
     },
   },
 }
